ea8928e2b5ffe9b35b80467e4b91573cef498b45
Yablargo <matthew.galligan@gmail.com>
2023-03-24 16:58:22 -0400

    Create fake_payload.sh

---
+++ fake_payload.sh
@@ -0,0 +1,112 @@
+Skip to content
+ 
+Search…
+All gists
+Back to GitHub
+@Yablargo 
+rce.py
+No description.
+problems.py
+99 problems solution with angr
+pwnit
+No description.
+pwncore.py
+No description.
+View your gists
+Gist description…
+test.sh
+
+Spaces
+
+2
+
+No wrap
+21
+echo "     $$      $iW$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$!         "
+22
+echo "     $$i      $$$$$$$#"" `"""#$$$$$$$$$$$$$$$$$#""""""#$$$$$$$$$$$$$$$W         "
+23
+echo "     #$$W    `$$$#"            "       !$$$$$`           `"#$$$$$$$$$$#         "
+24
+echo "      $$$     ``                 ! !iuW$$$$$                 #$$$$$$$#          "
+25
+echo "      #$$    $u                  $   $$$$$$$                  $$$$$$$~          "
+26
+echo "       "#    #$$i.               #   $$$$$$$.                 `$$$$$$         "
+27
+echo "              $$$$$i.                """#$$$$i.               .$$$$#          "
+28
+echo "              $$$$$$$$!         .   `    $$$$$$$$$i           $$$$$         "
+29
+echo "              `$$$$$  $iWW   .uW`        #$$$$$$$$$W.       .$$$$$$#          "
+30
+echo "                "#$$$$$$$$$$$$#`          $$$$$$$$$$$iWiuuuW$$$$$$$$W         "
+31
+echo "                   !#""    ""             `$$$$$$$##$$$$$$$$$$$$$$$$          "
+32
+echo "              i$$$$    .                   !$$$$$$ .$$$$$$$$$$$$$$$#          "
+33
+echo "             $$$$$$$$$$`                    $$$$$$$$$Wi$$$$$$#"#$$`         "
+34
+echo "             #$$$$$$$$$W.                   $$$$$$$$$$$#   ``         "
+35
+echo "              `$$$$##$$$$!       i$u.  $. .i$$$$$$$$$#""          "
+36
+echo "                 "     `#W       $$$$$$$$$$$$$$$$$$$`      u$#          "
+37
+echo "                                W$$$$$$$$$$$$$$$$$$      $$$$W          "
+38
+echo "                                $$`!$$$##$$$$``$$$$      $$$$!          "
+39
+echo "                               i$" $$$$  $$#"`  """     W$$$$         "
+40
+echo "                                                       W$$$$!         "
+41
+echo "                          uW$$  uu  uu.  $$$  $$$Wu#   $$$$$$         "
+42
+echo "                         ~$$$$iu$$iu$$$uW$$! $$$$$$i .W$$$$$$         "
+43
+echo "                 ..  !   "#$$$$$$$$$$##$$$$$$$$$$$$$$$$$$$$#"         "
+44
+echo "                 $$W  $     "#$$$$$$$iW$$$$$$$$$$$$$$$$$$$$$W         "
+45
+echo "                 $#`   `       ""#$$$$$$$$$$$$$$$$$$$$$$$$$$$         "
+46
+echo "                                  !$$$$$$$$$$$$$$$$$$$$$#`          "
+47
+echo "                                  $$$$$$$$$$$$$$$$$$$$$$!         "
+48
+echo "                                $$$$$$$$$$$$$$$$$$$$$$$`          "
+49
+echo "                                 $$$$$$$$$$$$$$$$$$$$"          "
+50
+​
+51
+echo "RUNNING EVIL PAYLOAD!!!"
+52
+whoami
+53
+id
+54
+ifconfig
+55
+​
+Create secret gist
+Secret gists are hidden by search engines but visible to anyone you give the URL to.
+Create public gist
+Public gists are visible to everyone.
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
+Create a new Gist
